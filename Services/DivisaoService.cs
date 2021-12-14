05640774a7923ecc8b9b5cf9eb78dfbe96850067
FabyGithub7481 <96150599+FabyGithub7481@users.noreply.github.com>
2021-12-14 17:51:25 -0500

    Update DivisaoService.cs

--- Services/DivisaoService.cs
+++ Services/DivisaoService.cs
@@ -7,6 +7,7 @@ namespace Calculadora.Services
 	{
 		public ECodigoOperacao CodigoOperacao => ECodigoOperacao.Divisao;
 
+		//manejar excepcion division para 0
 		public decimal Execute(IRequestViewModel request) => request.Numero1 / request.Numero2;
 	}
 }
